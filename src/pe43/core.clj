e81095e70e25c3883ff75704c13e62607bdd50c0
Kenny <kenny@Kenny.attlocal.net>
2015-08-11 23:34:14 -0400

    Moved function declaration to top level.

--- src/pe43/core.clj
+++ src/pe43/core.clj
@@ -1,26 +1,30 @@
 (ns pe43.core
   (:gen-class))
 
+
+(defn pandigital0-9? [number]
+  (let [numstring (str number)]
+    (if (= 10 (count numstring))
+      (loop [i 0]
+        (when (.contains numstring (str i))
+          (if (= i 9)
+            true
+            (recur (inc i))))))))
+
+(defn has-weird-property? [number]
+  (let [numstring (str number)]
+    (and (= 0 (mod (. Integer parseInt (subs numstring 1 4)) 2))
+         (= 0 (mod (. Integer parseInt (subs numstring 2 5)) 3))
+         (= 0 (mod (. Integer parseInt (subs numstring 3 6)) 5))
+         (= 0 (mod (. Integer parseInt (subs numstring 4 7)) 7))
+         (= 0 (mod (. Integer parseInt (subs numstring 5 8)) 11))
+         (= 0 (mod (. Integer parseInt (subs numstring 6 9)) 13))
+         (= 0 (mod (. Integer parseInt (subs numstring 7 10)) 17)))))
+
+
 (defn -main
   [& args]
-  (defn pandigital0-9? [number]
-    (let [numstring (str number)]
-      (if (= 10 (count numstring))
-        (loop [i 0]
-          (when (.contains numstring (str i))
-            (if (= i 9)
-              true
-              (recur (inc i))))))))
-
-  (defn has-weird-property? [number]
-    (let [numstring (str number)]
-      (and (= 0 (mod (. Integer parseInt (subs numstring 1 4)) 2))
-           (= 0 (mod (. Integer parseInt (subs numstring 2 5)) 3))
-           (= 0 (mod (. Integer parseInt (subs numstring 3 6)) 5))
-           (= 0 (mod (. Integer parseInt (subs numstring 4 7)) 7))
-           (= 0 (mod (. Integer parseInt (subs numstring 5 8)) 11))
-           (= 0 (mod (. Integer parseInt (subs numstring 6 9)) 13))
-           (= 0 (mod (. Integer parseInt (subs numstring 7 10)) 17)))))
+  
 
 ;  Test function calls
  (println (pandigital0-9? 1406357289))
@@ -34,5 +38,4 @@
        (if (< i 9876543211)
          (recur (inc i))
          (println total))))
-  )
-
+   )
